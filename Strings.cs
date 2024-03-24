b65cae25920a766394cbfba6f2edbd0c649b50ff
Xeno <1145764+MercuriusXeno@users.noreply.github.com>
2024-03-23 19:51:11 -0500

    Strings tweaks
    Version bump, nothing important.

--- Strings.cs
+++ Strings.cs
@@ -4,8 +4,8 @@
     {
         public const string HealthRegenRateLabel = "Health Regen Rate";
         public const string StaminaRegenRateLabel = "Stamina Regen Rate";
-        public const string HealthRegenRateDescription = "The percent of max health you regen each second";
-        public const string StaminaRegenRateDescription = "The percent of max stamina you regen each second";
+        public const string HealthRegenRateDescription = "The percent of your maximum health you regenerate each second";
+        public const string StaminaRegenRateDescription = "The percent of your maximum stamina you regenerate each second";
         public static string CaseyConfigMenuApi = "spacechase0.GenericModConfigMenu";
     }
 }
